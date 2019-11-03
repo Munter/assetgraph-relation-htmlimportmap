d991a494a5a92b49a9e4f20ea3e2070022aee4ca
Peter Müller <munter@fumle.dk>
2019-11-03 17:15:34 +0100

    Initial commit

---
+++ test/HtmlImportMap.test.js
@@ -0,0 +1,6 @@
+const HtmlImportMap = require('../');
+const AssetGraph = require('assetgraph');
+
+AssetGraph.registerRelation(HtmlImportMap, 'HtmlImportMap');
+
+describe('HtmlImportMap relation', () => {});
